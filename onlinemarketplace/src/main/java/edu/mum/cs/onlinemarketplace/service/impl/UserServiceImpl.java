6b4ed4809dd1116b6214cdc17fd057a027c8e3ca
Diego Kramer <diego.kramer99@gmail.com>
2019-08-13 23:05:09 -0500

    Ads and dynamic code added to to home page

--- onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/service/impl/UserServiceImpl.java
+++ onlinemarketplace/src/main/java/edu/mum/cs/onlinemarketplace/service/impl/UserServiceImpl.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -26,14 +27,18 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User getUserById(Long id) {
-        return userRepository.findById(id).get();
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()) return user.get();
+        return null;
     }
 
 
 
     @Override
     public User findUserById(Long id) {
-        return userRepository.findById(id).get();
+        Optional<User> user = userRepository.findById(id);
+        if(user.isPresent()) return user.get();
+        return null;
     }
 
     @Override
